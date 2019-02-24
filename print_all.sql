7f8459be9973f3325bf1475792d0de83de072699
Laura Greenstreet <k1j8@thetis.ugrad.cs.ubc.ca>
2019-02-24 14:00:30 -0800

    Saved copy of database to text file.

---
+++ print_all.sql
@@ -0,0 +1,17 @@
+/* Prints all 12 tables */
+
+set linesize 350;
+set pagesize 350;
+
+select * from PostalCode;
+select * from HealthcareProfessional;
+select * from Doctor;
+select * from Pharmacist;
+select * from LabTechnician;
+select * from Patient;
+select * from Referral;
+select * from Prescription;
+select * from LabTest;
+select * from ProvincialHealthPlan;
+select * from ExtendedBenefitsPlan;
+select * from Invoice;
