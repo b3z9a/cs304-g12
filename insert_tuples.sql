4120abb0b8669c667fc9285e18f9af96d39e230b
Laura Greenstreet <laura.greenstreet@gmail.com>
2019-03-23 11:25:06 -0700

    Implemented getTests and getReferrals

--- insert_tuples.sql
+++ insert_tuples.sql
@@ -47,7 +47,7 @@ insert into LabTechnician values (53647, 864527);
 insert into LabTechnician values (53176, 896264);
 
 insert into Patient values (32118954, 'Jonathan', 'Banks', '2144 Dunbar St.', 'V6B3N6', '604-731-2499', '778-456-2382');
-insert into Patient values (31294771, 'Christopher', 'Ellis', 'E-104-N, Pinehurst Apartments, 422 Maple St', 'V6B3N6', NULL, NULL);
+insert into Patient values (12345678, 'Chris', 'Ellis', 'E-104-N, Pinehurst Apartments, 422 Maple St', 'V6B3N6', NULL, NULL);
 insert into Patient values (29562774, 'David', 'Chen', '1114 Lameys Mill Rd.', 'V6S0J9', '604-731-8753', '604-731-9187');
 insert into Patient values (31857372, 'Winny', 'Zhu', '4491 W 4th Ave', 'V6S0J9', '778-291-3753', '778-981-3722');
 insert into Patient values (31274656, 'Antar', 'Atwal', 'Apt. 117, 8833 Hazelbridge Way', 'V6Y2V7', '604-233-4713', '604-233-8172');
@@ -55,10 +55,12 @@ insert into Patient values (32485737, 'Alex', 'Grossman', '271 Pike St.', '98226
 insert into Patient values (31572742, 'Mary', 'Cornwall', '4532 Lakeview Way', 'T5A0A1', '', '780-555-4031');
 
 insert into Referral values (32118954, 52918, 54337, TO_DATE('2019-02-21', 'YYYY-MM-DD'));
-insert into Referral values (31294771, 52918, 52731, TO_DATE('2018-05-01', 'YYYY-MM-DD'));
+insert into Referral values (12345678, 52918, 52731, TO_DATE('2018-05-01', 'YYYY-MM-DD'));
 insert into Referral values (32485737, 52918, 52731, TO_DATE('2019-02-18', 'YYYY-MM-DD'));
 insert into Referral values (31857372, 52918, 53833, TO_DATE('2018-12-11', 'YYYY-MM-DD'));
 insert into Referral values (31274656, 52918, 53833, TO_DATE('2018-10-06', 'YYYY-MM-DD'));
+insert into Referral values (12345678, 52918, 53833, TO_DATE('2018-12-11', 'YYYY-MM-DD'));
+insert into Referral values (12345678, 52918, 54337, TO_DATE('2019-02-21', 'YYYY-MM-DD'));
 
 insert into Medication values ('Albuterol', 'mg');
 insert into Medication values ('Esomeprazole', 'mg');
@@ -67,20 +69,22 @@ insert into Medication values ('Fluoxetine', 'mg');
 insert into Medication values ('Pregabalin', 'mL');
 
 insert into Prescription values (452855, 'Albuterol', 25, 100, 32118954, 54337, 55246, TO_DATE('2018-09-11','YYYY-MM-DD'), TO_DATE('2018-09-13','YYYY-MM-DD'));
-insert into Prescription values (485274, 'Fluoxetine', 20, 300, 31294771, 52918, 53763, TO_DATE('2019-01-20','YYYY-MM-DD'), TO_DATE('2019-01-20','YYYY-MM-DD'));
-insert into Prescription values (438245, 'Esomeprazole', 10, 40, 31294771, 52918, 53763, TO_DATE('2018-08-04','YYYY-MM-DD'), NULL);
+insert into Prescription values (485274, 'Fluoxetine', 20, 300, 12345678, 52918, 53763, TO_DATE('2019-01-20','YYYY-MM-DD'), TO_DATE('2019-01-20','YYYY-MM-DD'));
+insert into Prescription values (438245, 'Esomeprazole', 10, 40, 12345678, 52918, 53763, TO_DATE('2018-08-04','YYYY-MM-DD'), NULL);
+insert into Prescription values (472596, 'Pregabalin', 85, 1, 12345678, 52918, 52127, TO_DATE('2018-03-15','YYYY-MM-DD'), TO_DATE('2018-03-21','YYYY-MM-DD'));
+insert into Prescription values (492845, 'Fluticasone', 15, 100, 12345678, 52918, 52127, TO_DATE('2018-04-26','YYYY-MM-DD'), TO_DATE('2018-05-01','YYYY-MM-DD'));
 insert into Prescription values (472947, 'Pregabalin', 85, 1, 31857372, 52918, 52127, TO_DATE('2018-03-15','YYYY-MM-DD'), TO_DATE('2018-03-21','YYYY-MM-DD'));
 insert into Prescription values (427437, 'Fluticasone', 15, 100, 31274656, 52918, 52127, TO_DATE('2018-04-26','YYYY-MM-DD'), TO_DATE('2018-05-01','YYYY-MM-DD'));
 
-insert into LabTest values (827432, 175.4, 70.8, 95.2, 104.6, 2.27, 3.51, 45.1, 195, 25.7, 0.75, 140, 3.8, 133.2, 31294771, 52918, 51833, TO_DATE('2019-01-29', 'YYYY-MM-DD'), NULL);
-insert into LabTest values (882475, 132.0, 70.2, 61.8, 85.7, 2.33, 3.46, 40.2, 206, 17.2, 0.66, 140, 3.6, 121.0, 31294771, 52918, 52856, TO_DATE('2019-02-23', 'YYYY-MM-DD'), NULL);
-insert into LabTest values (830485, 155.7, 85.7, 70.0, 113.2, 2.36, 3.58, 39.7, 173, 23.3, 0.70, 140, 3.7, 140.6, 31294771, 52918, 53176, TO_DATE('2018-11-05', 'YYYY-MM-DD'), TO_DATE('2018-11-06', 'YYYY-MM-DD'));
-insert into LabTest values (865282, 200.8, 103.9, 96.9, 92.7, 2.21, 3.44, 48.3, 180, 20.1, 0.68, 140, 4.1, 135.9, 31294771, 52918, 53647, TO_DATE('2018-10-21', 'YYYY-MM-DD'), TO_DATE('2018-10-23', 'YYYY-MM-DD'));
-insert into LabTest values (818673, 125.0, 63.5, 61.5, 99.6, 2.42, 3.20, 42.9, 215, 21.5, 0.72, 140, 4.0, 144.6, 31294771, 52918, 54328, TO_DATE('2018-12-13', 'YYYY-MM-DD'), TO_DATE('2018-12-17', 'YYYY-MM-DD'));
+insert into LabTest values (827432, 175.4, 70.8, 95.2, 104.6, 2.27, 3.51, 45.1, 195, 25.7, 0.75, 140, 3.8, 133.2, 12345678, 52918, 51833, TO_DATE('2019-01-29', 'YYYY-MM-DD'), NULL);
+insert into LabTest values (882475, 132.0, 70.2, 61.8, 85.7, 2.33, 3.46, 40.2, 206, 17.2, 0.66, 140, 3.6, 121.0, 12345678, 52918, 52856, TO_DATE('2019-02-23', 'YYYY-MM-DD'), NULL);
+insert into LabTest values (830485, 155.7, 85.7, 70.0, 113.2, 2.36, 3.58, 39.7, 173, 23.3, 0.70, 140, 3.7, 140.6, 12345678, 52918, 53176, TO_DATE('2018-11-05', 'YYYY-MM-DD'), TO_DATE('2018-11-06', 'YYYY-MM-DD'));
+insert into LabTest values (865282, 200.8, 103.9, 96.9, 92.7, 2.21, 3.44, 48.3, 180, 20.1, 0.68, 140, 4.1, 135.9, 12345678, 52918, 53647, TO_DATE('2018-10-21', 'YYYY-MM-DD'), TO_DATE('2018-10-23', 'YYYY-MM-DD'));
+insert into LabTest values (818673, 125.0, 63.5, 61.5, 99.6, 2.42, 3.20, 42.9, 215, 21.5, 0.72, 140, 4.0, 144.6, 12345678, 52918, 54328, TO_DATE('2018-12-13', 'YYYY-MM-DD'), TO_DATE('2018-12-17', 'YYYY-MM-DD'));
 
 insert into ProvincialHealthPlan values (128473, 'BC Resident - MSP', TO_DATE('2018-01-01','YYYY-MM-DD'), TO_DATE('2019-12-31','YYYY-MM-DD'), 32118954);
 insert into ProvincialHealthPlan values (125724, 'BC Resident - MSP', TO_DATE('2018-01-01','YYYY-MM-DD'), TO_DATE('2019-12-31','YYYY-MM-DD'), 29562774);
-insert into ProvincialHealthPlan values (127828, 'BC Resident - Income Assistance', TO_DATE('2019-01-01','YYYY-MM-DD'), TO_DATE('2019-12-31','YYYY-MM-DD'), 31294771);
+insert into ProvincialHealthPlan values (127828, 'BC Resident - Income Assistance', TO_DATE('2019-01-01','YYYY-MM-DD'), TO_DATE('2019-12-31','YYYY-MM-DD'), 12345678);
 insert into ProvincialHealthPlan values (121738, 'Out-of-Province', TO_DATE('2019-01-01','YYYY-MM-DD'), TO_DATE('2019-12-31','YYYY-MM-DD'), 31274656);
 insert into ProvincialHealthPlan values (126724, 'Out-of-Province', TO_DATE('2019-01-01','YYYY-MM-DD'), TO_DATE('2019-12-31','YYYY-MM-DD'), 31274656);
 
@@ -91,7 +95,10 @@ insert into ExtendedBenefitsPlan values (274633, 127828, TO_DATE('2018-01-01', '
 insert into ExtendedBenefitsPlan values (258291, 121738, TO_DATE('2019-01-01', 'YYYY-MM-DD'), TO_DATE('2020-01-01', 'YYYY-MM-DD'), 'N', 'N', 'N', 'N', 'N', 'Y', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2500.00, 0.00);
 
 insert into Invoice values (119274, 32118954, 'Annual exam', TO_DATE('2018-11-02','YYYY-MM-DD'), TO_DATE('2018-12-02','YYYY-MM-DD'), 'Paid', TO_DATE('2018-11-27','YYYY-MM-DD'), 'Credit\Debit', 0.0, 628462, 128473);
+insert into Invoice values (183746, 12345678, 'Annual exam', TO_DATE('2018-11-02','YYYY-MM-DD'), TO_DATE('2018-12-02','YYYY-MM-DD'), 'Paid', TO_DATE('2018-11-27','YYYY-MM-DD'), 'Credit\Debit', 0.0, 628462, 128473);
 insert into Invoice values (148924, 32485737, 'Clinic visit', TO_DATE('2018-10-14','YYYY-MM-DD'), TO_DATE('2018-11-14','YYYY-MM-DD'), 'Unpaid', NULL, NULL, 200.0, NULL, 121738);
+insert into Invoice values (182747, 12345678, 'Clinic visit', TO_DATE('2018-10-14','YYYY-MM-DD'), TO_DATE('2018-11-14','YYYY-MM-DD'), 'Unpaid', NULL, NULL, 200.0, NULL, 121738);
 insert into Invoice values (152748, 31572742, 'Clinic visit', TO_DATE('2018-05-11','YYYY-MM-DD'), TO_DATE('2018-05-11','YYYY-MM-DD'), 'Paid', TO_DATE('2018-11-27','YYYY-MM-DD'), 'Cheque', 0.0, 628462, 126724);
 insert into Invoice values (119396, 31857372, 'Orthodic insoles', TO_DATE('2019-01-13','YYYY-MM-DD'), TO_DATE('2019-03-13','YYYY-MM-DD'), 'Unpaid', NULL, NULL, 265.50, NULL, 128473);
+insert into Invoice values (137472, 12345678, 'Orthodic insoles', TO_DATE('2019-01-13','YYYY-MM-DD'), TO_DATE('2019-03-13','YYYY-MM-DD'), 'Unpaid', NULL, NULL, 265.50, NULL, 128473);
 insert into Invoice values (192745, 29562774, 'Massage re: back injury', TO_DATE('2019-01-04','YYYY-MM-DD'), TO_DATE('2019-02-04','YYYY-MM-DD'), 'Unpaid', NULL, NULL, 145.0, NULL, 127828);
