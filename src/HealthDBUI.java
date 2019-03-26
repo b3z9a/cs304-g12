e9858f1763cb87ddb8b42f06bf48f1a65265917c
Jan Evangelista <a.c.husso@gmail.com>
2019-03-26 11:09:58 -0700

    Update View test dialog

--- src/HealthDBUI.java
+++ src/HealthDBUI.java
@@ -878,6 +878,20 @@ public class HealthDBUI extends JFrame {
         btnViewTest.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                JTextField dChol = new JTextField();
+                JTextField dHDL = new JTextField();
+                JTextField dLDL = new JTextField();
+                JTextField dTrig = new JTextField();
+                JTextField dWBcc = new JTextField();
+                JTextField dRBcc = new JTextField();
+                JTextField dHema = new JTextField();
+                JTextField dPlate = new JTextField();
+                JTextField dNRPer = new JTextField();
+                JTextField dNRAbs = new JTextField();
+                JTextField dSod = new JTextField();
+                JTextField dGlu = new JTextField();
+                JTextField dPhos = new JTextField();
+                JTextField dLabHID = new JTextField();
 
                 if(patientArray.size() > 0) {
                     String name = patientArray.get(0) + " " + patientArray.get(1);
@@ -888,13 +902,29 @@ public class HealthDBUI extends JFrame {
                         String testID = testPSTableModel.getValueAt(row, 0).toString();
                         ArrayList<String> testData = hdb.findTest(testID);
 
-                        Object[] data = {"Patient: " + name, "Test ID: " + testID, " ", "Cholesterol: " + testData.get(0), "HDL Cholesterol: " + testData.get(1),
-                                "LDL Choleterol: " + testData.get(2), "Triglycerides: " + testData.get(3), "White Blood Cell Count: " + testData.get(4),
-                                "Red Blood Cell Count: " + testData.get(5), "Hematocrit: " + testData.get(6), "Platelet Count: " + testData.get(7),
-                                "NRBC Percent: " + testData.get(8), "NRBC Absolute: " + testData.get(9), "Sodium: " + testData.get(10),
-                                "Glucose: " + testData.get(11), "Phosphorus: " + testData.get(12), "Lab Tech ID: " + testData.get(13)};
+                        dChol.setText(testData.get(0));
+                        dHDL.setText(testData.get(1));
+                        dLDL.setText(testData.get(2));
+                        dTrig.setText(testData.get(3));
+                        dWBcc.setText(testData.get(4));
+                        dRBcc.setText(testData.get(5));
+                        dHema.setText(testData.get(6));
+                        dPlate.setText(testData.get(7));
+                        dNRPer.setText(testData.get(8));
+                        dNRAbs.setText(testData.get(9));
+                        dSod.setText(testData.get(10));
+                        dGlu.setText(testData.get(11));
+                        dPhos.setText(testData.get(12));
+                        dLabHID.setText(testData.get(13));
 
+                        Object[] data = {"Patient: " + name, "Test ID: " + testID, " ",
+                                "Cholesterol", dChol, "HDL Cholesterol", dHDL, "LDL Choleterol", dLDL,
+                                "Triglycerides", dTrig, "White Blood Cell Count", dWBcc, "Red Blood Cell Count", dRBcc,
+                                "Hematocrit", dHema, "Platelet Count", dPlate, "NRBC Percent", dNRPer,
+                                "NRBC Absolute", dNRAbs, "Sodium", dSod, "Glucose", dGlu,
+                                "Phosphorus", dPhos, "Lab Tech ID", dLabHID};
 
+                        String s = testData.get(0);
                         JOptionPane.showMessageDialog(frame, data, "Test # " + testID + " for Patient " + name, JOptionPane.PLAIN_MESSAGE);
                     }
                     else {
